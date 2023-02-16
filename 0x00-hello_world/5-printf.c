3f6c5538947fa51efceb041fdd3beed6efdb5992
Yeezy <you@you.com>
2023-02-16 14:32:39 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program thatprints a line using the print function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+	return (0);
+}
