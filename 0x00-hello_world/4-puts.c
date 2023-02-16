c4781f49e5b930471aaa37cb0d53405fcfbe7b65
Yeezy <you@you.com>
2023-02-16 14:47:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include < stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line function
- * return: 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
