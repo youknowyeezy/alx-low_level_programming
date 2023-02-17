e09f4399e09e482620c617d1fe2983bf627658bd
Yeezy <you@you.com>
2023-02-17 14:32:02 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+/**
+ * main - Prints Alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar("\n");
+
+	return (0);
+}
