3454f8295f7136efb7cafd9597ecc89ad37cdd5e
Yeezy <you@you.com>
2023-02-23 12:07:22 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers 0 to 10
+ * Return: Numbers 0 to 10
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x+ '0');
+	}
+	_putchar('\n');
+}
