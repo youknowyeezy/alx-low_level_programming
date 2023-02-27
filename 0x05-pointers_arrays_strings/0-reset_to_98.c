92393cc618f311bfd15e321d70e2a9183a87d6ac
Yeezy <you@you.com>
2023-02-27 06:57:00 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * reset_to_98 - Turns pointer to int and updates value to 98
+ * @n: AN int input pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
