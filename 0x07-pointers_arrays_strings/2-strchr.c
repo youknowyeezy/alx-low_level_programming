d1775f1b8ff0df0bb1f3061d8d37dd2cda2cb87e
Yeezy <you@you.com>
2023-03-06 14:52:08 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+/**
+ * _strchr - Entry point.
+ * @s: Input.
+ * @c: Input.
+ * Return: Always 0 (Success).
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
