70c036eaa6730beab724d11f1549a63ffdde007b
Kanye <tyopokhanyisa35@gmail.com>
2023-03-30 11:30:47 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+
+#define LISTS_H
+
+/**
+ * struct list_s - Singly linked list.
+ * @str: String - (malloc'ed string).
+ * @len: Length of the string.
+ * @next: Points to the next node.
+ * Description: Singly linked list node structure
+ * for Holberton project.
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
